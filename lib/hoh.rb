31eadf78ff7d1bbf82faa80d524734cab1c80a2c
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-03-23 22:31:42 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -48,7 +48,7 @@ def naming_system
             label: "Genus",
             sub_category: {
               label: "Species",
-              sub_category:{}
+              sub_category:nil
             }
           }
         }
